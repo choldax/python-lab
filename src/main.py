f48b043fb3a53cd0d468994b79a40cbc962dceaa
Abraham Chol Ayuen <choldax51@gmail.com>
2026-09-02 11:36:10 +0300

    Add personalized greeting feature

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from utils import square, is_even, celsius_to_fahrenheit
+from utils import square, is_even, celsius_to_fahrenheit, greet
+name = input("Enter your name: ")
+print(greet(name))
 
 
 number = float(input("Enter a number: "))
